3e134901b910c9d6b7b27a7d7d98b12c6b0fb1db
Rosalito Gabriel II <rosalito.gabriel@gmail.com>
2019-01-30 21:42:38 +0800

    Create a C# Application that accepts a user input, then display it in the console application.

--- CSharpApplication_Exercise2/Program.cs
+++ CSharpApplication_Exercise2/Program.cs
@@ -10,6 +10,21 @@ namespace CSharpApplication_Exercise2
     {
         static void Main(string[] args)
         {
+            //Create a C# Application that accepts a user input, then display it in the console application.
+            //Let's Begin! :)
+
+            string userInput; //First let us declare a string variable.
+            Console.Write("Please input a text: "); //Let us put a message that would prompt for a user input.
+            userInput = Console.ReadLine(); //This reads the user input and store it into our declared variable.
+            Console.WriteLine("You have inputed a text: " + userInput);
+
+            Console.ReadKey();
+            //We forgot to add a User KeyPress, so that the system would wait before exiting.
+
+            //Let us run our system by Pressing F5 or clicking the Start Button above.
+            //Let us try again. :)
+
+            //We have done it! :) Happy coding!
         }
     }
 }
